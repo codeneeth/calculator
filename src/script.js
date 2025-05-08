8c81fc3c5a7b1ad9776bb482e03865747d065553
Praneeth / <kanchetipraneethsai@gmail.com>
2025-05-08 19:30:01 +0530

    initial commit

---
+++ src/script.js
@@ -0,0 +1,63 @@
+// var my_Name = "slayer";
+// console.log(my_Name);
+
+// var myAge = 20;
+// console.log(myAge);
+
+// let $cityName = "hyderabad";
+// console.log($cityName);
+
+// var my@Email = 'an@cgmail.com';
+// console.log(my@Email);
+
+// var myName = 1
+// console.log(myName);
+// console.log(typeof myName);;
+
+
+// let x =10;
+// let y = 20;
+// console.log(x*y);
+
+// let result = "hello" / 2;
+// console.log(result );
+
+// let result = 0.1 + 0.2;
+// console.log(result);
+
+// let username;
+// document.getElementById("mySumbit"  ).onclick = function() {
+//     username = document.getElementById("myText").value;
+   
+// }
+
+
+// let a = "hello" + 2;
+// console.log( a);
+
+// if (false) {
+//     let x = 10;
+//     console.log(x);
+// }
+
+// console.time("myTimer");
+
+// console.log('hello wolrd');
+
+// console.log(36+8);
+// console.log([73,83,84]);
+// console.table ({name: "slayer", age : 20});
+// console.warn ("this is warning");
+// alert("hello")
+// console.error("error")
+// console.timeEnd("myTimer");
+
+
+// let varAp = String('83');
+// console.log(varAp);
+
+// let myNum = 30;
+// console.log(myNum.toString);
+
+let date = String (new Date()) ;
+console.log( date);
